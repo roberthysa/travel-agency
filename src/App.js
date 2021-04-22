9028ecbb3fc42222ee75be7cc66055250e189fb9
Robert Hysa <robert.hysa@wp.pl>
2021-04-22 20:58:56 +0200

    add routing and views Countries&Regions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
+
 // TODO - import other views
+import Trip from './components/views/Trip/TripContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import Country from './components/views/Country/CountryContainer';
+import Regions from './components/views/Regions/RegionsContainer';
+
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -40,7 +46,13 @@ class App extends React.Component {
           <Switch location={location}>
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
+
             {/* TODO - add more routes for other views */}
+            <Route exact path='/trip/:id' component={Trip}/>
+            <Route exact path='/countries' component={Countries}/>
+            <Route exact path='/country/:id' component={Country}/>
+            <Route exact path='/regions' component={Regions}/>
+
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
