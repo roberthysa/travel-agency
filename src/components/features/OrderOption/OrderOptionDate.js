471b4f3df7f231d2b915a4754bcb599e2ff17828
Robert Hysa <robert.hysa@wp.pl>
2021-04-30 16:16:24 +0200

    Add mentor sugestion to OrderOptionDate

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-const OrderOptionDate = (props) => {
+const OrderOptionDate = ({setOptionValue}) => {
   const [startDate, setStartDate] = useState(new Date());
 
   return (
@@ -13,7 +13,7 @@ const OrderOptionDate = (props) => {
       dateFormat="dd/MM/yyyy"
       onChange={date =>  {
         setStartDate(date);
-        props.setOptionValue(date);
+        setOptionValue(date);
       }
       }
     />
