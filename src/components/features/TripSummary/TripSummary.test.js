59d0d6471fc58dca7a3c362700346368b2e994e2
Robert Hysa <robert.hysa@wp.pl>
2021-05-04 23:22:38 +0200

    add test for TripSummary v2 - in progress

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -36,4 +36,20 @@ describe('Component TripSummary', () => {
   it('should throw error without required props', () => {
     expect(() => shallow(<TripSummary />)).toThrow();
   });
+
+  it('should render tags in spans in correct order', () => {
+    const expectedTags = ['tag1', 'tag1', 'tag3'];
+    const component = shallow(<TripSummary tags={expectedTags} />);
+
+    expect(component.find('.tags span').at(0).text()).toEqual(expectedTags[0]);
+    expect(component.find('.tags span').at(1).text()).toEqual(expectedTags[1]);
+    expect(component.find('.tags span').at(2).text()).toEqual(expectedTags[2]);
+  });
+
+  it('should not render div .tags without props tags or tags=[]', () => {
+    const component = shallow(<TripSummary />);
+    expect(component.find('.tags')).toMatchObject({});
+    const component2 = shallow(<TripSummary tags={[]} />);
+    expect(component2.find('.tags')).toMatchObject({});
+  });
 });
