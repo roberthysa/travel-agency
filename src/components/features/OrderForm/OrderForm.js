0522643faa756ec2042b1fcd3f61763fb13a43bb
Robert Hysa <robert.hysa@wp.pl>
2021-05-05 20:19:51 +0200

    add file settings.js and button to OrderForm

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,37 @@ import {Row, Col} from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = ({tripCost, options, setOrderOption}) => (
   <Row>
@@ -15,6 +46,7 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options}/>
     </Col>
+    <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
   </Row>
 );
 
